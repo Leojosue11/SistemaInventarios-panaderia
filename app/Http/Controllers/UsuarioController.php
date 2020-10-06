d694ff13a20f891ddc120a6dd599d1dce5707823
camaleoncoce <42813694+camaleoncoce@users.noreply.github.com>
2020-10-06 17:34:52 -0600

    borrar el order by

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -27,7 +27,7 @@ class UsuarioController extends Controller
                'rols.NombreRol',
                'rols.IdRol'
             )
-            ->orderByDesc("id")
+          
             ->get();
         return $Usuarios;
     }
