2840a773b4a2d1642228d70f9745dd5d3a1804e4
Leo Josué <leojosue2205@gmail.com>
2020-11-05 21:33:18 -0600

    Se cambio funcionalidad al hacer salidas de materia prima, ahora resta de las entradas mas prontas a vencer

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Pedido;
 use App\Models\inventario;
+use App\Models\MateriaPrimaProveedor;
 use App\Http\Controllers\Controller;
 use App\Models\MovimientosMP;
 use Illuminate\Http\Request;
@@ -65,54 +66,82 @@ class PedidoController extends Controller
         ], $messages);
 
         //Si falla la validacion
-
-        if ($validator->fails()) 
-        {
+        if ($validator->fails()) {
             $errores = $validator->errors();
 
             return response()->json($errores, 402);
-        } 
+        }
 
-            // Sino existe un registro en bodega de la Materia Prima lo crea, sino lo actualiza
-            // $Inventario = inventario::where('RegistroMPID', $MovimientoMateriaPrima["RegistroMPID"])->first();
-            $Inventario = inventario::where('RegistroMPID', $request["RegistroMPID"])->first();
+        // Sino existe un registro en bodega de la Materia Prima lo crea, sino lo actualiza
+        $Inventario = inventario::where('RegistroMPID', $request["RegistroMPID"])->first();
 
-            if ($Inventario == false) 
-            {
-                $message=array('No puede hacer movimiento porque no hay entradas de Materia Prima');
-                return response()->json([
-                     'msg'=>$message,
-                 ],402);
+        if ($Inventario == false) {
+            $message = array('No puede hacer movimiento porque no hay entradas de Materia Prima');
+            return response()->json([
+                'msg' => $message,
+            ], 402);
+        } elseif ($request["CantidadPedido"] > $Inventario["Disponible"]) {
+            $message = array('No hay disponibilidad suficiente en Inventario. Disponibilidad: ' . $Inventario["Disponible"]);
+            return response()->json([
+                'msg' => $message,
+            ], 402);
+        }
 
-            } elseif($request["CantidadPedido"] > $Inventario["Disponible"]) 
-            {
-                $message=array('No hay disponibilidad suficiente en Inventario. Disponibilidad:'.$Inventario["Disponible"]);
-                return response()->json([
-                     'msg'=>$message,
-                 ],402);
-            }
+        //Buscamos las entradas de la Materia Prima, ordenado por fecha de caducidad
+        $Entrada = MateriaPrimaProveedor::where('MateriaPrimaID', $request["RegistroMPID"])
+            ->orderBy('FechaCaducidad', 'asc')
+            ->first();
+        $CantidadActual = $Entrada["CantidadTotal"];
+        $IdRegistro = $Entrada["IDMatPrimaProveedor"];
 
-             //Guarda los Movimientos a las Sucursales
-             $MovimientoMateriaPrima = Pedido::create($request->all());
-             //Crea un registro en el historial de Movimientos
-             MovimientosMP::Create([
-                 'MateriaPrimaID' => $MovimientoMateriaPrima["RegistroMPID"],
-                 'Cantidad' => $MovimientoMateriaPrima["CantidadPedido"],
-                 'FechaMovimiento' => today(),
-                 'SucursalID' => $MovimientoMateriaPrima["SucursalID"]
-             ]);
-            //Hace la Resta del inventario - Salida 
-            $Disponible = 0;
-            $Disponible =  $this->Restar_Disponibilidad($Inventario["Disponible"], $MovimientoMateriaPrima["CantidadPedido"]);
+        //Restar Cantidad de la entrada mas pronta a vencer
+        $Salida = $this->Restar_Entrada($CantidadActual, $request["CantidadPedido"], $IdRegistro);
 
-            //Actualiza Disponibilidad
-            DB::table('inventarios')->where('RegistroMPID', $MovimientoMateriaPrima["RegistroMPID"])
-                ->update([
-                    'Disponible' => $Disponible
-                ]);
+        //Si hizo falta de la entrada mas pronta a vencer (Salida = 0 No falta)
+        if ($Salida <> 0) {
+            //Asigna el valor faltante a una variable a ocupar en el loop
+            $sobrante = $Salida;
+            do {
+                // Trae la siguiente entrada mas pronta a vencer 
+                $Entrada = MateriaPrimaProveedor::where('MateriaPrimaID', $request["RegistroMPID"])
+                    ->where('CantidadTotal', '<>', 0)
+                    ->orderBy('FechaCaducidad', 'asc')
+                    ->first();
+                $CantidadActual = $Entrada["CantidadTotal"];
+                $IdRegistro = $Entrada["IDMatPrimaProveedor"];
 
-            return '{"msg":"creado","result":' . $MovimientoMateriaPrima . '}';
-       
+                //Resta lo faltante de la salida a realizar
+                $Faltante = $this->Restar_Entrada($CantidadActual, $sobrante, $IdRegistro);
+                $sobrante = $Faltante;
+            } while ($sobrante <> 0);
+        }
+        // Pruebas
+        // return response()->json([
+        //     'Inicio' => $Actual,
+        //     'Actualizado' => $Salida
+        // ], 402); 
+
+        //Guarda los Movimientos a las Sucursales
+        $MovimientoMateriaPrima = Pedido::create($request->all());
+        //Crea un registro en el historial de Movimientos
+        MovimientosMP::Create([
+            'MateriaPrimaID' => $MovimientoMateriaPrima["RegistroMPID"],
+            'Cantidad' => $MovimientoMateriaPrima["CantidadPedido"],
+            'FechaMovimiento' => today(),
+            'SucursalID' => $MovimientoMateriaPrima["SucursalID"]
+        ]);
+        
+        //Hace la Resta del inventario - Salida 
+        $Disponible = 0;
+        $Disponible =  $this->Restar_Disponibilidad($Inventario["Disponible"], $MovimientoMateriaPrima["CantidadPedido"]);
+
+        //Actualiza Disponibilidad
+        DB::table('inventarios')->where('RegistroMPID', $MovimientoMateriaPrima["RegistroMPID"])
+            ->update([
+                'Disponible' => $Disponible
+            ]);
+
+        return '{"msg":"creado","result":' . $MovimientoMateriaPrima . '}';
     }
 
     /**
@@ -138,17 +167,16 @@ class PedidoController extends Controller
     {
         //forma 1
         //$IdPedido->update($request->all());
-       
-            //Actualiza informacion en Pedidos
-            $pedido = Pedido::find($IdPedido);
-            $pedido->RegistroMPID = $request->input("RegistroMPID");
-            $pedido->CantidadPedido = $request->input("CantidadPedido");
-            $pedido->DescripcionPedido = $request->input("DescripcionPedido");
-            $pedido->BodegaID = $request->input("BodegaID");
-            $pedido->SucursalID = $request->input("SucursalID");
-            $pedido->save();
-            return response()->json($pedido);
 
+        //Actualiza informacion en Pedidos
+        $pedido = Pedido::find($IdPedido);
+        $pedido->RegistroMPID = $request->input("RegistroMPID");
+        $pedido->CantidadPedido = $request->input("CantidadPedido");
+        $pedido->DescripcionPedido = $request->input("DescripcionPedido");
+        $pedido->BodegaID = $request->input("BodegaID");
+        $pedido->SucursalID = $request->input("SucursalID");
+        $pedido->save();
+        return response()->json($pedido);
     }
     /**
      * Remove the specified resource from storage.
@@ -166,5 +194,25 @@ class PedidoController extends Controller
         $Cantidad = ($CantidadInv - $SalidaBod);
         return $Cantidad;
     }
- 
+    public function Restar_Entrada($CantidadEntrada, $Salida, $EntradaID)
+    {
+        $Cantidad = 0;
+        $Cantidad = ($CantidadEntrada - $Salida);
+
+        if ($Cantidad < 0) {
+            MateriaPrimaProveedor::find($EntradaID)
+                ->update([
+                    'CantidadTotal' => 0
+                ]);
+            $Faltante = 0;
+            $Faltante = $Salida - $CantidadEntrada;
+            return $Faltante;
+        } else {
+            MateriaPrimaProveedor::find($EntradaID)
+                ->update([
+                    'CantidadTotal' => $Cantidad
+                ]);
+            return 0;
+        }
+    }
 }
